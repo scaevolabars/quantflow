fb6f451d3242ea3781d63cd351e4ecc19fe95944
Sergey Bars <ssbars137@gmail.com>
2023-10-15 22:17:46 +0400

    init

---
+++ build/CMakeFiles/QUANTFLOW.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QUANTFLOW.dir/src/main.cpp.o"
+  "CMakeFiles/QUANTFLOW.dir/src/main.cpp.o.d"
+  "QUANTFLOW"
+  "QUANTFLOW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QUANTFLOW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
